bee2e6939a1c3e1203e4371b6336ff92d5a5c382
MikhailKorotkov1992 <105540007+MikhailKorotkov1992@users.noreply.github.com>
2022-06-08 20:56:42 +0300

    Исправил ошибки в моделях БД, создал страницу добавления места в БД (#2)
    * Исправил ошибки в моделях БД, создал страницу добавления места в БД
    
    * Вытащил config

--- allabouttravel_app/models.py
+++ allabouttravel_app/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Country(db.Model):
     __tablename__ = 'countries'
 
@@ -10,27 +11,26 @@ class Country(db.Model):
     cities = db.relationship('City')
 
     def __repr__(self):
-        return f'Страна: {self.title}'
+        return f'{self.title}'
 
 
 class City(db.Model):
     __tablename__ = 'cities'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, unique=True, nullable=False)
     country_id = db.Column(db.Integer, db.ForeignKey('countries.id'))
-    country = db.relationship('Country')
     places = db.relationship('Place')
 
     def __repr__(self):
-        return f'Город: {self.title}'
+        return f'{self.title}'
 
 
 class Place(db.Model):
     __tablename__ = 'places'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, unique=True, nullable=False)
     description = db.Column(db.Text)
     is_verified = db.Column(db.Boolean, default=False, nullable=False)
     city_id = db.Column(db.Integer, db.ForeignKey('cities.id'))
@@ -40,11 +40,11 @@ class Place(db.Model):
         return f'{self.title}'
 
 
-class Categoty(db.Model):
+class Category(db.Model):
     __tablename__ = 'categories'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, unique=True, nullable=False)
     places = db.relationship('Place')
 
     def __repr__(self):
